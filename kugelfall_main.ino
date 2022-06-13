b3a428344c8edee6c8c8eee9a67c8e8669f008e5
chaikozova <49871708+chaikozova@users.noreply.github.com>
2022-06-13 22:56:24 +0200

    Update kugelfall_main.ino

--- kugelfall_main.ino
+++ kugelfall_main.ino
@@ -1,11 +1,11 @@
-#include "kugelfall_labo.h"
+#include "kugelfall_ISR.h"
 
-const int fallingTime = 495;
 const int fast[] = {1, 1, 1, 0};
 const int medium[] = {1, 0, 1, 1};
 const int slow[] = {1, 0, 1, 0};
+const int wait[] = {0, 0, 0, 0};
 
-const int patternSelcetion[] = {fast, medium, slow};
+const int patternSelcetion[] = {wait, fast, medium, slow};
 
 
 void setup() {
@@ -16,11 +16,45 @@ void setup() {
 void loop() {
 
 
-  if(isClicked()) {
+  if (isClicked()) {
     Serial.println("Button was clicked");
-    bool patternDone = false;
+    isDone = false;
 
-    
+    int velocityModeStart = velocityMode;
+
+    for (int i = 0; i <= 4 && isDone == false; i++) {
+
+      if (velocityMode != velocityModeStart) {
+        Serial.print("The velocity Mode has been changed.");
+        Serial.println("Please click the button if you want to star again");
+        isDone = true;
+        break;
+      }
+
+      switch (patternSelcetion[velocityMode]) {
+        case 0: {
+            Serial.println("Waiting for velocityMode have been known.");
+            break;
+          }
+
+        case 1: {
+            Serial.println("velocityMode is fast");
+            fireBall(patternSelcetion[velocityMode], i);
+            break;
+          }
+        case 2: {
+            Serial.println("velocityMode is medium");
+            fireBall(patternSelcetion[velocityMode], i);
+            break;
+          }
+        case 3: {
+            Serial.println("velocityMode is slow");
+            fireBall(patternSelcetion[velocityMode], i);
+            break;
+          }
+      }
+    }
+
 
   }
 }
